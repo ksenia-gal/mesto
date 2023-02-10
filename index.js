d1acfae9151c5f186c81e3b416d51898d6d0b6b4
Ксения Галкина <k5eniiagalkina@yandex.ru>
2023-02-10 19:52:32 +0300

    10/02

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const editPopup = document.querySelector('edit-popup')
 const editProfileButton = document.querySelector('.profile__edit-button');
 editProfileButton.addEventListener('click', function() {
     const editPopup = document.querySelector('.popup');
     console.log(editPopup);
-    editPopup.removeAttribute('display:none');
+    editPopup.classList.add('popup_opened');
+});
+
+const editPopupCloseButton = document.querySelector('.popup__close-button');
+closeProfileButton.addEventListener('click', function() {
+        editPopup.classList.remove('popup_opened');
 });
