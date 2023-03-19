58400eed38f392ab4a661730d36781c4dc5c4139
Ксения Галкина <k5eniiagalkina@yandex.ru>
2023-03-19 17:49:25 +0300

    19 03

--- scripts/index.js
+++ scripts/index.js
@@ -104,15 +104,23 @@ const popup = document.querySelector(".popup");
 const popupAdd = document.querySelector(".popup_type_add");
 const buttonAddProfile = document.querySelector(".profile__add-button");
 const buttonClosePopupAdd = popupAdd.querySelector(".popup__close-button");
-const popupOverlay = popup.querySelector('.popup__overlay');
-
+const OverlayList = Array.from(document.querySelectorAll('.popup__overlay'));
+const popupOverlay = popup.querySelectorAll('.overlay');
 function popupCloseByEsc(evt) {
   if (evt.key === 'Escape') {
     const popupOpened = document.querySelector('.popup_opened');
-    closePopup(popup);
+    closePopup(popupOpened);
   }
 }
 
+// // 3. Закрытие попапа кликом на оверлей
+
+
+OverlayList.forEach((overlay) => {
+  overlay.addEventListener('click', () => {
+    closePopup(popup)
+  });
+    });
 
 function openPopup(popup) {
   popup.classList.add("popup_opened");
@@ -164,77 +172,3 @@ buttonClosePopupZoom.addEventListener("click", function () {
   closePopup(popupZoom);
 });
 
-//1. Валидация формы «Редактировать профиль»
-
-// Вынесем все необходимые элементы формы в константы
-const formElement = document.querySelector('.popup__container');
-const formInput = formElement.querySelector('.popup__input');
-const formError = formElement.querySelector(`.${formInput.id}-error`);
-
-
-// Функция, которая добавляет класс с ошибкой
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add('popup__input_type_error');
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__input-error_active');
-};
-
-// // Функция, которая удаляет класс с ошибкой
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('popup__input_type_error');
-  errorElement.classList.remove('popup__input-error_active');
-  errorElement.textContent = '';
-};
-
-// // Функция, которая проверяет валидность поля
-const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    // Если поле не проходит валидацию, покажем ошибку
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    // Если проходит, скроем
-    hideInputError(formElement, inputElement);
-  }
-};
-
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-  return !inputElement.validity.valid;
-  }); 
-  }
-  const buttonElement = formElement.querySelector('.popup__submit');
-const toggleButtonState = (inputList, buttonElement) => {
-  if (hasInvalidInput(inputList)) {
-buttonElement.classList.add('popup__submit_disabled');
-} else {
-buttonElement.classList.remove('popup__submit_disabled');
-} 
-};
-
-
-const setEventListeners = (formElement) => {
-const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
-
-toggleButtonState(inputList, buttonElement);
-
-inputList.forEach((inputElement) => {
-inputElement.addEventListener('input', () => {
-checkInputValidity(formElement, inputElement);
-toggleButtonState(inputList, buttonElement);
-});
-});
-};
-
-
-const enableValidation = () => {
-const formList = Array.from(document.querySelectorAll('.popup__container'));
-formList.forEach((formElement) => {
-  formElement.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-  });
-setEventListeners(formElement);
-});
-};
-enableValidation();
