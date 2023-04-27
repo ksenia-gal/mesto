21fdd3da182049456132b86d7366028e9d3edd81
Ksenia Galkina <k5eniiagalkina@tandex.ru>
2023-04-27 10:46:34 +0300

    dc

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,4 +1,4 @@
-import { Popup } from "../../src/components/Popup.js";
+import { Popup } from "../components/Popup.js";
 export class PopupWithForm extends Popup {
   constructor(popupSelector, callbackSubmit) {
     super(popupSelector)
@@ -28,42 +28,3 @@ export class PopupWithForm extends Popup {
     })
   }
 }
-/*// наследуется от Popup, вызывает его конструктор, в который передает нужный параметр.
-// При этом принимает еще и второй параметр - колбэк сабмита формы.
-export class PopupWithForm extends Popup {
-  constructor( popupSelector, {handleFormSubmit }) {
-    super(popupSelector);
-    this._formElement = this._popupSelector.querySelector(".popup__container");
-    this._handleFormSubmit = handleFormSubmit;
-  }
-
-// будет метод getinputvalues кот будет брать зн-я с инпутов - соберешь инф-ю по полям(что пользователь 
-// ввел) и запихнешь эти значения в сабмит
-  _getInputValues() {
-    this._inputList = this._popupSelector.querySelectorAll(".popup__input");
-    this._formValues = {};
-    this._inputList.forEach(input => 
-        this._formValues[input.name] = input.value);
-    return this._formValues;
-  }
-
-  // seteventlisteners будет другой - повесить до слушатеь - сабмит на кнопку
-  setEventListeners() {
-    super.setEventListeners();
-    this._popupSelector.addEventListener("submit", () => {
-      this._handleFormSubmit(this._getInputValues());
-      this.close();
-    });
-  }
-
-  // close будет другой (исп-я super; при закрытии нужно сбрасывать форму)
-  close() {
-    super.close();
-    this._formElement.reset();
-  }
-}
-
-// Создаем два экземпляра этого класса, в каждый передаем свой коллебек (помимо селектора попапа).
-//  В одном случае форма редактирует данные пользователя на странице, во втором - добавляет новую карточку.
-//  В качестве идеи - попробуйте совместить функцию коллбека при сабмите формы
-//  добавления карточки с аргументом renderer у класса Section*/
---
+++ src/utils/constants.js
@@ -0,0 +1,67 @@
+const cards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    alt: "Фотография гор",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    alt: "Фотография зимней реки",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    alt: "Фотография панельного дома",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    alt: "Фотография природы Камчатки",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    alt: "Фотография железной дороги",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    alt: "Фотография природы Байкала",
+  },
+]
+const popups = document.querySelectorAll(".popup")
+const editPopup = document.querySelector(".popup_type_edit")
+const profileForm = document.forms["profile-form"]
+const nameInput = document.querySelector(".popup__input_name")
+const jobInput = document.querySelector(".popup__input_information")
+const profileName = document.querySelector(".profile__title")
+const profileAdditionalInfo = document.querySelector(".profile__subtitle")
+const popupAdd = document.querySelector(".popup_type_add")
+const popupZoom = document.querySelector(".popup_type_zoom")
+const buttonEditProfile = document.querySelector(".profile__edit-button")
+const inputFullName = document.querySelector(".popup__input_name")
+const inputAdditionalInfo = document.querySelector(".popup__input_information")
+const elements = document.querySelector(".elements")
+const popupImage = popupZoom.querySelector(".popup__image")
+const popupHeading = popupZoom.querySelector(".popup__caption")
+const buttonAddProfile = document.querySelector(".profile__add-button")
+const placeForm = document.forms["place-form"]
+const formInputPlaceName = placeForm.querySelector(".popup__input_place-name")
+const formInputPlaceLink = placeForm.querySelector(".popup__input_image")
+const buttonClosePopupProfile = editPopup.querySelector(".popup__close-button")
+const buttonClosePopupAdd = popupAdd.querySelector(".popup__close-button")
+const buttonClosePopupZoom = popupZoom.querySelector(".popup__close-button")
+const buttonSubmitPlace = popupAdd.querySelector(".popup__submit_create")
+const buttonSubmitEdit = editPopup.querySelector(".popup__submit_save")
+const cardTemplateSelector = document.querySelector(".card")
+const config = {
+  formSelector: ".popup__container",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__submit",
+  inactiveButtonClass: "popup__submit_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__input-error_active",
+}
+
+export { cards, config, editPopup, popupAdd, buttonEditProfile, placeForm, nameInput, jobInput, buttonAddProfile, formInputPlaceName, formInputPlaceLink, elements }
